56f1064855725d62630a6dfcb9b1750db82dfbc9
Richard van Dusschoten <richardd@mediamonks.com>
2024-08-10 21:58:56 +0200

    all the things

---
+++ Source/Fifiablo/Fifiablo.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fifiablo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fifiablo, "Fifiablo" );
+
+DEFINE_LOG_CATEGORY(LogFifiablo)
+ 
---
+++ Source/Fifiablo/FifiabloGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FifiabloGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFifiabloGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFifiabloGameMode();
+};
+
+
+
